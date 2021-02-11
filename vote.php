0fb76e28d70e76b01228be81f658b1d7cc5de688
Tincho <tincholaguna37@gmail.com>
2021-02-11 19:33:26 -0400

    Clean code on vote.php

--- vote.php
+++ vote.php
@@ -17,123 +17,76 @@
         <div class="main">
             <?php
                 include 'db_connection.php';
-
                 $conn = OpenCon();
 
                 // Get ? Post maybe?
                 $school = $_GET['school'];
 
-                // Conseguir la escuela por el resultado del GET del index.php
-                switch ($school) {
-                    case "DTI":
-                      $validDegrees = array();
-                      break;
-                    case "MEE":
-                      $validDegrees = array();
-                      break;
-                    case "EIE":
-                      $validDegrees = array();
-                      break;
-                    case "ADMI":
-                      $validDegrees = array();
-                      break;
-                    case "ICO":
-                      $validDegrees = array();
-                      break;
-                    case "COMU":
-                      $validDegrees = array();
-                      break;
-                    case "CSJ":
-                      $validDegrees = array();
-                      break;
-                    case "DISEÑO":
-                      $validDegrees = array();
-                      break;
-                    case "FINAN":
-                      $validDegrees = array();
-                      break;
-                    case "MKT":
-                      $validDegrees = array();
-                      break;
-                    default:
-                      echo "Your school is not valid!";
-                }
-
                 // Crear el form para poder ingresar codigo y votar
-                // TODO remover flechas y verificar que se valide solo numeros
                 // TODO verificar que el codigo no tenga menos o mas de 5 caracteres
-                echo "<form method='post'>";
-                echo "<h2>Candidatos de ". $school . " - ¡A votar! </h2>";
-                echo "Tu Código: <input type='number' name='code' id='code' value='";
-                print $_POST["code"];
-                echo "' placeholder='Ej.: 10101'>";
+            ?>
+                <form method='POST'>
+            <?php 
+                // Comenzar el proceso de votacion llenando el formulario para introducir el codigo
+                // (Mas abajo, en el else mas grande)
+                $formCodigo = <<<EOT
+                        <label for="code">Tu Código:</label> 
+                        <input type='number' name='code' id='code' value='' placeholder='Ej.: 10101'>
+                        <button type='submit' name='CheckBtn' class='buttonVote buttonVote1'>Verificar</button><br>
+                    </form>
+                EOT;
 
-                echo " <button type='submit' name='CheckBtn' class='buttonVote buttonVote1'>Verificar</button><br>";
 
-                // Verificacion del form anterior ^^^
-
+                // Verificacion (de los datos) del form anterior ^^^
                 if(isset($_POST["CheckBtn"])) {
-                    // Consigue codigo y nombre completo del estudiante y verifica si no ha votado
                     $code = $_POST["code"];
-
-                    // Bool en vez de tinyint en voted?
-
-                    $sql3 = "SELECT id FROM students WHERE id = $code AND voted = '0'";
-                    $result3 = $conn->query($sql3);
-
-                    $sql4 = "SELECT fullname FROM students WHERE id = $code AND voted = '0'";
-                    $result4 = $conn->query($sql4);
-
-                    if ($result3 == null) {
-                        echo "Por favor introduzca su codigo";
-                    } else {
-                        // 
-                        if ($result3->num_rows > 0) {
-                			// output data of each row
-                			while($row = $result3->fetch_assoc()) {
+                    // Primero verificar si el codigo que se dio de la anterior pagina existe en la BD
+                    $result = $conn->query("SELECT id FROM students WHERE id = $code");
+                    if ($result->num_rows > 0) {
+                        // Segundo verificar si el estudiante ya voto
+                        $result2 = $conn->query("SELECT id FROM students WHERE id = $code AND voted = 0");
+                        if ($result2->num_rows > 0) {
+                            // Consigue codigo y nombre completo del estudiante y verifica si no ha votado
+                            while($row = $result2->fetch_assoc()) {
                                 echo "Habilitado para Votar <br>";
                                 echo "Código: " . $row["id"] . "<br>";
-                                
-                                if ($result4->num_rows > 0){
-                                    while($row = $result4->fetch_assoc()) {
-                                        echo "Nombre: " . $row["fullname"];
-                                        echo "<br><br><br>";
-                                        echo "<h2>CANDIDATOS:<h2> <br>";
-                                    }
+                            }
+
+                            $sql3 = "SELECT fullname FROM students WHERE id = $code AND voted = 0";
+                            $result3 = $conn->query($sql3);
+
+                            if ($result3->num_rows > 0){
+                                while($row = $result3->fetch_assoc()) {
+                                    echo "Nombre: " . $row["fullname"];
+                                    echo "<br><br><br>";
+                                    echo "<h2>CANDIDATOS:<h2> <br>";
                                 }
+                            }
 
-                                // TODO Imagenes para cada capi
-                                $sql2 = "SELECT fullname FROM candidates WHERE degree = '$school'";
-                                $result2 = $conn->query($sql2);
+                            // TODO Imagenes para cada capi
+                            $sql4 = "SELECT fullname FROM candidates WHERE degree = '$school'";
+                            $result4 = $conn->query($sql4);
 
-                                // TODO Cont? -> bool mejor?
-                                $cont = 0;
-
-                                if ($result2->num_rows > 0) {
-                                    // output data of each row
-                                    echo "<table class='table'>";
-                                    while($row = $result2->fetch_assoc()) {
-                                        if ($cont == 0){
-                                            echo "<tr><th><input type='radio' class='radio' name='candidate' value='" . $row["fullname"] . "'> " . $row["fullname"] . "</th>";
-                                            $cont = 1;
-                                        } else if ($cont == 1) {
-                                            echo "<th><input type='radio' class='radio' name='candidate' value='" . $row["fullname"] . "'> " . $row["fullname"] . "</th></tr>";
-                                            $cont = 0;
-                                        }
-                                    }
-                                } else {
-                                    echo "No se encontraron resultados";
+                            // Se imprimen los candidatos
+                            if ($result4->num_rows > 0) {
+                                echo "<table class='table'>";
+                                while($row = $result4->fetch_assoc()) {
+                                    echo "<tr><th><input type='radio' class='radio' name='candidate' value='" . $row["fullname"] . "'> " . $row["fullname"] . "</th></tr>";
                                 }
-
                                 echo "</table><br><br>";
+                                echo "<input type='hidden' name='code' value='$code'>";
                                 echo "<button type='submit' name='VoteBtn' class='buttonVote buttonVote1'>Votar</button><br>";
-                	       }
+
+                            } else {
+                                echo "No se encontraron resultados";
+                            }
                         } else {
-                    			echo "Vayase porfavor";
+                			echo "El estudiante con este código ya votó.";
                 		}
+                    } else {
+                        echo "No existe ningún estudiante con este código.";
                     }
 
-                    // TODO cambiar el if-else-if para que sea un solo formulario Y se actualicen los datos.
                 } else if (isset($_POST['VoteBtn'])) {
                     $std0 = $_POST["code"];
                     $std = (int)$std0;
@@ -141,15 +94,16 @@
                     if(!isset($_POST['candidate'])) {
                         echo "Selecciona una opción.";
                         echo $std;
+
                     } else {
                         $cand = $_POST['candidate'];
                         $sqlvote = "UPDATE candidates SET votes = votes + 1 WHERE fullname = '$cand'";
                         $sqlvoteCheck = "UPDATE students SET voted = 1 WHERE id = $std";
 
-                        if ($conn->query($sqlvote) === TRUE) {
-                            echo "Gracias por votar!";
+                        if ($conn->query($sqlvote)) {
+                            echo "Gracias por votar!<br>";
 
-                            if($conn->query($sqlvoteCheck) === TRUE) {
+                            if($conn->query($sqlvoteCheck)) {
                                 echo "Votaste por " . $cand . " codigo " . $std;
                             }
 
@@ -157,9 +111,10 @@
                             echo "Error: " . $sqlvote . "<br>" . $conn->error;
                         }
                     }
+                } else {
+                    // Primera fase del formulario, donde se introduce el codigo (CheckBtn no esta setteado, ni VoteBtn)
+                    echo $formCodigo;
                 }
-                
-                echo "</form>";
 
                 CloseCon($conn);
             ?>
